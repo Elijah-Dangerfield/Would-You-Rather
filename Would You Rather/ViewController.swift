dafc164a72be9f77c86098509d3f3f6ca886a5a2
Elijah Dangerfield <elijahdangerfield111@gmail.com>
2019-04-29 16:47:59 -0500

    Initial Commit

---
+++ Would You Rather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Would You Rather
+//
+//  Created by eli dangerfield on 4/29/19.
+//  Copyright © 2019 eli dangerfield. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
